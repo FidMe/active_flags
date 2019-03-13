fdfcdd4b3fd1984e5d2f683cdd622762c670fc4e
Nathan Huberty <nathan.huberty@orange.fr>
2019-03-13 16:32:09 +0100

    Refacto FlagBuilder.

--- lib/active_flags/handler/flag_builder.rb
+++ lib/active_flags/handler/flag_builder.rb
@@ -7,21 +7,9 @@ module ActiveFlags
       end
 
       def save
-        flag = ActiveFlags::Flag.find_by(subject: @resource, key: @flag_attributes[:key])
-        flag = flag ? update_flag(flag) : create_flag
-        @resource.flags_as_collection << flag
-      end
-
-      def update_flag(flag)
-        flag.tap do |flag|
-          flag.update!(value: @flag_attributes[:value])
-        end
-      end
-
-      def create_flag
-        ActiveFlags::Flag.new(subject: @resource, key: @flag_attributes[:key]) do |flag_to_create|
-          flag_to_create.value = @flag_attributes[:value]
-        end
+        ActiveFlags::Flag.find_or_initialize_by(subject: @resource, key: @flag_attributes[:key])
+                         .update!(value: @flag_attributes[:value])
+        @resource.reload
       end
     end
   end
